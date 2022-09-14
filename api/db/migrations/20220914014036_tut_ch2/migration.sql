676baf9668608a078d615e797daf3d90180aa007
Kevin Sparling <kevinsparling@gmail.com>
2022-09-14 10:36:41 +0800

    working on tutorial

---
+++ api/db/migrations/20220914014036_tut_ch2/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "hashedPassword" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+    "resetToken" TEXT NOT NULL,
+    "resetTokenExpiresAt" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
--- web/src/Routes.js
+++ web/src/Routes.js
@@ -9,15 +9,23 @@
 
 import { Router, Route, Set } from '@redwoodjs/router'
 
+import PostsLayout from 'src/layouts/PostsLayout'
+
 import ChakraLayout from './layouts/ChakraLayout/ChakraLayout'
 
 const Routes = () => {
   return (
     <Router>
+      {/* <Set wrap={PostsLayout}>
+      </Set> */}
       <Set wrap={ChakraLayout}>
         <Route path="/" page={HomePage} name="home" />
         <Route notfound page={NotFoundPage} />
         <Route path="/about" page={AboutPage} name="about" />
+        <Route path="/posts" page={PostPostsPage} name="posts" />
+        <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
+        <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
+        <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
         <Route path="/login" page={LoginPage} name="login" />
         <Route path="/signup" page={SignupPage} name="signup" />
         <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
